ce34ea6ed0ee0924a1027cb8e4580bedf529b760
112916-Mateo-Salas <111440978+112916-Mateo-Salas@users.noreply.github.com>
2024-11-01 16:19:27 -0300

    Se sube routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { ListOrdersComponent } from './list-orders/list-orders.component';
+import { CreateOrderComponent } from './create-order/create-order.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [{
+    path: 'orders', component: ListOrdersComponent
+},
+{
+    path:'create-order', component: CreateOrderComponent
+},
+{
+    path:'', component:CreateOrderComponent
+}];
